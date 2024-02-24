90fc44705b325d619c4358f54aa3299288510a8d
Tarun Dadlani <tarun3dadlani@gmail.com>
2024-02-24 01:03:53 -0500

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>This is a sample site</div>
+        <div>This is a sample site 1</div>
       </header>
     </div>
   );
